184f8aa44f10f6004eefda9270662c8306f0a53c
Luis Ariel RH <regalomessi10@gmail.com>
2022-06-09 13:06:00 -0500

    lanzamiento_v1

--- src/features/convertSlice.js
+++ src/features/convertSlice.js
@@ -1,16 +1,98 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+
 export const convertSlice = createSlice({
   name: 'convert',
   initialState: {
     value: '',
   },
   reducers: {
-    romanToDecimal: (state) => {
-      state.value = '123456789';
+    romanToDecimal: (state, action) => {
+      const conversion = {
+        M: 1000,
+        D: 500,
+        C: 100,
+        L: 50,
+        X: 10,
+        V: 5,
+        I: 1,
+      };  
+
+      const repetRomans = ['', 0]
+      let arr = action.payload.split("");
+      let total = 0;
+      let current, currentValue, next, nextValue;
+
+      for (let i = 0; i < arr.length; i++) {
+
+        current = arr[i];
+        currentValue = conversion[current];
+        next = arr[i + 1];
+        nextValue = conversion[next];
+
+        if(
+          currentValue + nextValue === 10 
+          || (current === 'V' && currentValue < nextValue)
+          ) return new Error("Invalid Roman Numeral");
+    
+        if (repetRomans[0] === current) repetRomans[1] += 1;
+        else {
+          repetRomans[0] = current;
+          repetRomans[1] = 0;
+        }
+        if (repetRomans[1] >= 4) return new Error("Invalid Roman Numeral");
+
+        
+        if (currentValue < nextValue) {
+          total -= currentValue;
+        } else {
+          total += currentValue;
+        }
+      } 
+      state.value = total;
     },
-    decimalToRoman: (state) => {
-      state.value = '12345';
+    decimalToRoman: (state, action) => {
+      let
+      values = [1, 5, 10, 50, 100, 500, 1000],
+      letras = ['I', 'V', 'X', 'L', 'C', 'D', 'M'],
+      res = [],
+      num, letra, val, pos, insert
+  
+    for(let i = 6; num = values[i], letra = letras[i]; i--) {
+      // Suficientemente grande
+      if(action.payload >= num) {
+        // Número de letras repetidas
+        let r = Math.floor(action.payload / num); 
+  
+        // Restamos el actual
+        action.payload -= r * num; 
+  
+        if(r < 4){
+          // Metemos las letras
+          while(r--){
+            res.push(letra);
+          }
+        } else {
+          // No se pueden repetir 4+ veces
+          val = res.pop(); // Última letra
+  
+          // Si es el string vacío o letra == "M" (no hay anterior)
+          // usamos la letra anterior a esta
+          pos = (val ? letras.indexOf(val) : i) + 1; 
+  
+          // Y si letra == "M" -> letras[pos] no existirá y usamos M
+          insert = letra + (letras[pos] || 'M'); 
+  
+          // Insertamos el string
+          res.push(insert);
+        }
+      } else {
+        // Si no vamos a poner letra usamos un ""
+        // para que no afecte pop
+        res.push('');
+      }
+    }
+      state.value = res.join('');
     }
   },
 })
