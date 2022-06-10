31703981c3e3ef3d3bde53148310f1f0f843e1a7
Luis Ariel RH <regalomessi10@gmail.com>
2022-06-09 19:30:17 -0500

    warning for romanos a decimal v1.2.0

--- src/features/convertSlice.js
+++ src/features/convertSlice.js
@@ -67,45 +67,36 @@ export const convertSlice = createSlice({
       res = [],
       num, letra, val, pos, insert
   
-    for(let i = 6; ; i--) {
-      num = values[i]
-      letra = letras[i]
-      // Suficientemente grande
-      if(action.payload >= num) {
-        // Número de letras repetidas
+      for(let i = 6; i >= 0;i--) {
+        num = values[i];
+        letra = letras[i];
+
+        if(action.payload >= num) {
         let r = Math.floor(action.payload / num); 
-  
-        // Restamos el actual
+    
         action.payload -= r * num; 
-  
+    
         if(r < 4){
-          // Metemos las letras
           while(r--){
             res.push(letra);
           }
         } else {
-          // No se pueden repetir 4+ veces
           val = res.pop(); // Última letra
-  
-          // Si es el string vacío o letra == "M" (no hay anterior)
-          // usamos la letra anterior a esta
           pos = (val ? letras.indexOf(val) : i) + 1; 
-  
-          // Y si letra == "M" -> letras[pos] no existirá y usamos M
+    
           insert = letra + (letras[pos] || 'M'); 
-  
-          // Insertamos el string
+    
           res.push(insert);
         }
-      } else {
-        // Si no vamos a poner letra usamos un ""
-        // para que no afecte pop
-        res.push('');
+        } else {
+      
+          res.push('');
+        }
       }
-    }
+  
       state.value = res.join('');
     }
-  },
+  }  
 })
 
 export const { romanToDecimal, decimalToRoman } = convertSlice.actions
